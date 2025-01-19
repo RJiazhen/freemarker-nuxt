bc2ac89ae7ece34f017b1918c44f0641120f0481
RJiazhen <jiazhen.ruan@outlook.com>
2025-01-19 13:00:17 +0800

    fix(nuxt.config.ts): freemarker error after build

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,18 @@
+import { realpathSync } from 'fs';
+import { copySync } from 'fs-extra';
+import { resolve } from 'path';
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui']
-})
+  modules: ['@nuxt/ui'],
+  hooks: {
+    'nitro:build:public-assets': () => {
+      const src = resolve(__dirname, 'node_modules/freemarker');
+      const realpath = realpathSync(src);
+      const dest = resolve(__dirname, '.output/server/node_modules/freemarker');
+      copySync(realpath, dest, { overwrite: true });
+    },
+  },
+});
